2f32b8964ce31d3103b6dcea67b042461d8d22ea
marko-0016 <87635867+marko-0016@users.noreply.github.com>
2021-08-15 17:11:19 +0900

    新規登録機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+ private
+ def configure_permitted_parameters  
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:email,:family_name,:first_name,:family_name_katakana,:first_name_katakana,:birthday])
+ end
 end
+
+# keysにパスワードは不要？
+# |encrypted_password  
+
+
+
+# フリマアプリ基本機能だけの場合はユーザーズコントローラーは作成しない。
+# 追加機能でユーザー詳細などを作る場合は生成する
