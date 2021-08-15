1ed6c35f13dbe0e6ca9fac908a30a4d55f3a3e9e
marko-0016 <87635867+marko-0016@users.noreply.github.com>
2021-08-15 11:06:24 +0900

    マイグレーションファイルにテーブル設計

--- db/migrate/20210815011325_devise_create_users.rb
+++ db/migrate/20210815011325_devise_create_users.rb
@@ -6,7 +6,20 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname, null: false
+      t.string :family_name, null: false
+      t.string :first_name, null: false
+      t.string :family_name_katakana, null: false
+      t.string :first_name_katakana, null: false
+      t.date :birthday, null: false
 
+      
+  # ## Association
+  # - has_many :items
+  # - has_many :purchases_histories
+
+
+  
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
