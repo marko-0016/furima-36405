5849c1b0b92df1be5226baa25613199d0e3db9b3
marko-0016 <87635867+marko-0016@users.noreply.github.com>
2021-08-16 12:25:12 +0900

    ログイン、ログアウト機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  
 
  private
  def configure_permitted_parameters  
@@ -11,7 +12,6 @@ end
 
 # keysにパスワードは不要？
 # |encrypted_password  
-
 
 
 # フリマアプリ基本機能だけの場合はユーザーズコントローラーは作成しない。
