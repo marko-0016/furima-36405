44adafed4cf8609f66fa30aa7f1b98224040a0a9
marko-0016 <87635867+marko-0016@users.noreply.github.com>
2021-08-17 15:46:11 +0900

    エラー修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :password
+    validates :family_name
+    validates :first_name
+    validates :family_name_katakana
+    validates :first_name_katakana
+    validates :birthday
+  end
  
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'エラー内容を後から記述'
@@ -8,14 +19,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates:nickname,presence:true
-  validates:email,presence:true
-  validates:password,presence:true, length: {minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
-  validates:family_name,presence:true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates:first_name,presence:true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates:family_name_katakana,presence:true,format: { with: /\A[ァ-ヶ一]+\z/ }
-  validates:first_name_katakana,presence:true,format: { with: /\A[ァ-ヶ一]+\z/ }
-                      validates:birthday,presence:true
+  validates :password, length: {minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
+  validates :family_name,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :family_name_katakana,format: { with: /\A[ァ-ヶ一]+\z/ }
+  validates :first_name_katakana,format: { with: /\A[ァ-ヶ一]+\z/ }
+
 
   # 必要な段階くるまで以下コメントアウトにしている
   # has_many :items
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,18 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
+    it "重複したemailが存在する場合登録できない" do
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+    end
+    it "emailは@を含まないと登録できない" do
+      @user.email = 'po12ae-gmail.com'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email is invalid")
+    end
     it "passwordが空では登録できない" do
       @user.password = ''
       @user.valid?
@@ -53,7 +65,7 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Family name can't be blank")
     end
     it "family_nameは漢字・平仮名・カタカナ以外では登録できない" do
-      @user.family_name = ''
+      @user.family_name = '111111'
       @user.valid?
       expect(@user.errors.full_messages).to include("Family name is invalid")
     end
@@ -63,7 +75,7 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("First name can't be blank")
     end
     it "first_nameは漢字・平仮名・カタカナ以外では登録できない" do
-      @user.first_name = ''
+      @user.first_name = '111111'
       @user.valid?
       expect(@user.errors.full_messages).to include("First name is invalid")
     end
@@ -88,12 +100,12 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("First name katakana can't be blank")
     end
     it "first_name_katakanaがカナ以外では登録できない" do
-      @user.first_name_katakana = ''
+      @user.first_name_katakana = '111111'
       @user.valid?
       expect(@user.errors.full_messages).to include("First name katakana is invalid")
     end
     it "first_name_katakanaがひらがなでは登録できない" do
-      @user.first_name_katakana = ''
+      @user.first_name_katakana = 'ああああああ'
       @user.valid?
       expect(@user.errors.full_messages).to include("First name katakana is invalid")
     end
