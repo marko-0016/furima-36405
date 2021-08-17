1302c68baa54bbc85d99312f886bc118a6573cf8
marko-0016 <87635867+marko-0016@users.noreply.github.com>
2021-08-17 16:32:49 +0900

    エラー修正３

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   end
  
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'エラー内容を後から記述'
+  validates_format_of :password, with: PASSWORD_REGEX,
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
