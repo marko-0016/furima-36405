7bea80031de916176c240b5fdc2ada79f8ba19e0
marko-0016 <87635867+marko-0016@users.noreply.github.com>
2021-08-20 16:41:59 +0900

    エラー修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,8 +12,9 @@ class Item < ApplicationRecord
 
     #ジャンルの選択が「--」の時は保存できないようにする
     validates :category_id,:condition_id,:delivery_fee_id,:shipping_area_id,:days_to_ship_id, numericality: { other_than: 0 , message: "can't be blank"}
-
+    #金額が300円以上9999999円以下のバリデーション
     validates :price,numericality: { only_integer:true, greater_than_or_equal_to:300, less_than_or_equal_to: 9999999 }
+
 
     with_options format: { with: /\A[0-9]+\z/ } do
       validates :price
@@ -35,5 +36,3 @@ end
 
   #  Association
   # has_one :purchase_history
-
-
