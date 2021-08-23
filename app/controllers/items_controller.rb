7223caaf8f0414348abc53bef8b9eb298a39cf50
marko-0016 <87635867+marko-0016@users.noreply.github.com>
2021-08-23 19:58:57 +0900

    エラー修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :create]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :set_user, only: [:edit, :update]
 
   def index
@@ -35,9 +35,10 @@ class ItemsController < ApplicationController
 end
 
 def destroy
-   @item = Item.find(params[:id])
+  if user_signed_in?
    @item.destroy
   redirect_to root_path
+end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'items#index'
-  resources :items, only: [:new, :index, :create, :show, :edit, :update, :destroy] 
-
+  resources :items 
 end
