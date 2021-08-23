fedf2018d08ff48f7ad6d74991a488156b593b25
marko-0016 <87635867+marko-0016@users.noreply.github.com>
2021-08-23 20:04:53 +0900

    エラー修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :update, :create]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :create, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :set_user, only: [:edit, :update]
 
@@ -35,7 +35,6 @@ class ItemsController < ApplicationController
 end
 
 def destroy
-  if user_signed_in?
    @item.destroy
   redirect_to root_path
 end
