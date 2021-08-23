aefb1c1f52db82844fb37865573e4859fc3a764b
marko-0016 <87635867+marko-0016@users.noreply.github.com>
2021-08-23 16:59:16 +0900

    エラー修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :create]
   before_action :set_item, only: [:show, :edit, :update]
   before_action :set_user, only: [:edit, :update]
-
 
   def index
     @items = Item.order("created_at DESC")
