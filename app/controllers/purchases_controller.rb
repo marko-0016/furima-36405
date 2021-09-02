15a3da494e04b1f164c1e8dfaeca1c12cd2a3d2e
marko-0016 <87635867+marko-0016@users.noreply.github.com>
2021-09-02 15:32:52 +0900

    エラー修正３

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,24 +2,23 @@ class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
-    binding.pry
     @purchase_purchases_history = PurchasePurchasesHistory.new
     @item = Item.find(params[:item_id])
-    
-     if @item.user == current_user
+     if @item.user == current_user || @item.purchases_history != nil
          redirect_to root_path
     end
   end
+
 
  def create
         @item = Item.find(params[:item_id])
        @purchase_purchases_history = PurchasePurchasesHistory.new(purchase_purchases_history_params)
    if  @purchase_purchases_history.valid?
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  
     Payjp::Charge.create(
-      amount: @item.price,  # 商品の値段
-      card: purchase_purchases_history_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+      amount: @item.price,  
+      card: purchase_purchases_history_params[:token], 
+      currency: 'jpy'     
     )
        @purchase_purchases_history.save
        redirect_to root_path
